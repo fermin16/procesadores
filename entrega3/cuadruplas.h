4f40ec6e40dbc7cd8938b1a147a2e2e77bf527d9
Yaiza <gomez.1071871@e.unavarra.es>
2020-11-25 18:35:04 +0100

    creo cuadruplas.h

---
+++ entrega3/cuadruplas.h
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define op_sumaEntero 1
+#define op_restaEntero 2
+#define op_sumaReal 3
+#define op_restaReal 4
+#define op_multiplicacionEntero 5
+#define op_multiplicacionReal 6
+#define op_divisionEntera 7
+#define op_divisionReal 8
+#define op_mod 9
+#define op_asignacion 10
+#define op_goto 11
+#define op_gotoMenor 12
+#define op_gotoMayor 13
+#define op_gotoMayorIgual 14
+#define op_gotoMenorIgual 15
+#define op_distinto 16
+#define op_gotoIgual 17
+#define op_gotoCond 18
+#define op_sumaEnteroUnario 19
+#define op_restaEnteroUnario 20
+#define op_sumaRealUnario 21
+#define op_restaRealUnario 22
+#define op_intToReal 23
+#define op_assignFalse 24
+#define op_assingTrue 25
+#define number_of_string 25
+#define max_string_size 100
