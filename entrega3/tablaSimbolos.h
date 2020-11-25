f38af455ac1e7272cfe88d3706fbc3a2eda07cce
Ferm'in Gasc'on <fermin1bachiller@gmail.com>
2020-11-25 19:14:48 +0100

    añado cosntantes para tippos de datos

--- entrega3/tablaSimbolos.h
+++ entrega3/tablaSimbolos.h
@@ -1,6 +1,13 @@
 #ifndef TABLA_SIM_H
 #define TABLA_SIM_H
 
+
+#define TIPO_ENTERO -1
+#define TIPO_REAL -2
+#define TIPO_CARACTER -3
+#define TIPO_CADENA -4
+#define TIPO_BOOLEANO -5
+
 typedef struct tablaSimbolos {
 	struct simbolo* inicio;
 	struct simbolo* end;
